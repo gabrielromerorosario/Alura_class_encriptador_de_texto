2af71923867c4242f5cb2add6cd7b953c0b33439
Gabriel <gabrielromero.gr.gr@gmail.com>
2022-09-24 22:07:18 -0400

    Codigo Funcioanl

--- encritar.js
+++ encritar.js
@@ -1,65 +1,98 @@
 var inputTexto = document.querySelector(".text_encriptar");
 var mensaje = document.querySelector(".mensajefinal");
-var frame_5 = document.querySelector(".FRAME_5");
+var franme_5 = document.querySelector(".FRAME_5");
+let btn_encritor = document.getElementById("boton_encriptar_id");
+let btn_desencritor = document.getElementById("btn_desencriptar_id");
+let btn_copy = document.getElementById("copiar");
 
-function btn_Encriptar(){
-   var textoEncriptado = encriptar(inputTexto.value);
-   mensaje.value = textoEncriptado;
-   frame_5.style.display="none"
-   mensaje.style.backgroundImage = "none"
-   inputTexto.value = ""
-   console.log(textoEncriptado)
+mensaje.style.display = "none";
+btn_copy.style.visibility = "hidden";
+
+
+
+function btn_Encriptar() {
+    franme_5.style.display = "none";
+    mensaje.style.display = "initial";
+    btn_copy.style.visibility = "visible";
+    var textoEncriptado = encriptar(inputTexto.value);
+    mensaje.value = textoEncriptado;
+    mensaje.style.backgroundImage = "none";
+    inputTexto.value = "";
+
+
 }
 
-function encriptar (string){
-    let matrizCodigo = [["e","enter"], ["i","imes"], ["a", "ai"], ["o","ober"], ["u","ufat"]]
+function encriptar(string) {
+    let matrizCodigo = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"]
+    ]
     let stringEncriptado = string.toLowerCase();
 
-    for (let i = 0; i < matrizCodigo.length; i++){
-        if (stringEncriptado.includes(matrizCodigo[i][0])){
-            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
-        }   
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptado.includes(matrizCodigo[i][0])) {
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
     }
 
-    return stringEncriptado;   
+    return stringEncriptado;
 
 }
 
-let btn_encritor = document.getElementById("boton_encriptar_id");
-btn_encritor.onclick = btn_Encriptar;
 
 
-function btn_desencriptar(){
+
+
+
+
+
+function btn_desencriptar() {
+    franme_5.style.display = "none"
+    mensaje.style.display = "initial";
+    btn_copy.style.visibility = "visible";
     var textoEncriptado = desencriptar(inputTexto.value);
     mensaje.value = textoEncriptado;
     mensaje.style.backgroundImage = "none"
     inputTexto.value = ""
-    console.log(textoEncriptado)
- }
- 
- function desencriptar (string){
-     let matrizCodigo = [["enter","e"], ["imes","i"], ["ai", "a"], ["ober","o"], ["ufat","u"]]
-     let stringEncriptado = string.toLowerCase();
- 
-     for (let i = 0; i < matrizCodigo.length; i++){
-         if (stringEncriptado.includes(matrizCodigo[i][0])){
-             stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
-         }   
-     }
- 
-     return stringEncriptado;   
- 
- }
- 
- let btn_desencritor = document.getElementById("btn_desencriptar_id");
- btn_desencritor.onclick = btn_desencriptar;
 
- function copy(){
-    var copiar = document.querySelector(".mensajefinal"); 
-    copiar.select();
-    document.designMode("copy");
-    alert("se copia");
 }
 
-let btn_copy = document.getElementById("copiar");
+function desencriptar(string) {
+    let matrizCodigo = [
+        ["enter", "e"],
+        ["imes", "i"],
+        ["ai", "a"],
+        ["ober", "o"],
+        ["ufat", "u"]
+    ]
+    let stringEncriptado = string.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptado.includes(matrizCodigo[i][0])) {
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+
+    return stringEncriptado;
+
+}
+
+
+
+
+function copy() {
+
+    mensaje.select();
+    document.execCommand("copy")
+    inputTexto.value = "";
+    inputTexto.focus();
+
+
+}
+
+btn_encritor.onclick = btn_Encriptar;
+btn_desencritor.onclick = btn_desencriptar;
 btn_copy.onclick = copy;
